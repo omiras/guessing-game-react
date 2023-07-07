4b7bef5d38cf005446aa253fb050d38f5c4dffc2
Oscar Miras <oscar.miras@gmail.com>
2023-07-07 09:32:21 +0200

    soltuion

--- src/App.jsx
+++ src/App.jsx
@@ -7,67 +7,71 @@ function generateRandomNumber() {
 
 function App() {
 
-  // lo usaremos para saber si el usuario ha introducido un numero más alto o más bajo. Además nos servirá si el usuario agota todos los intentos, para mostrarlo
-  const [randomNumber, setRandomNumber] = useState(generateRandomNumber())
+  // Variable de estado para indicar el número de intentos restantes
+  const [remainingGuesses, setRemainingGuesses] = useState(10);
 
-  // tendremos que decrementar uno cada vez que el usuario introduzca un número
-  const [remainingGuesses, setRemainingGuesses] = useState(2);
+  // Números probados hasta el momento
+  const [guesses, setGuesses] = useState([]);
 
-  // [4, 40, 20]
-  const [previousGuesses, setPreviousGuesses] = useState([]);
-
-  // del input
+  // input
   const [currentGuess, setCurrentGuess] = useState('');
 
-  // 'el número es menor' o 'el número es mayor'
-  const [message, setMessage] = useState('');
+  // Número correcto
+  const [correctNumber, setCorrectNumber] = useState(generateRandomNumber);
 
-  // ¿cual es el valor de randomNumber?
-  console.log(randomNumber);
-  console.log('Has cambiado el estado verdad ?? vuelvo a renderizar el componente');
-
-  const handleClick = () => {
-    const updatedGuesses = [...previousGuesses, +currentGuess];
-    setPreviousGuesses(updatedGuesses);
+  const handleNewGuess = () => {
+    // añadir la predicción al array de guesses
+    setGuesses([...guesses, currentGuess]);
+    // limpiar el input
     setCurrentGuess('');
-
-    if (currentGuess > randomNumber) {
-      setMessage('The number is lower');
-      setRemainingGuesses(remainingGuesses - 1);
-
-    } else if (currentGuess < randomNumber) {
-      setMessage('The number is higher');
-      setRemainingGuesses(remainingGuesses - 1);
-    }
-    // else ... entonces ya tenemos el número correcto!
-
-  };
-
-  // si la variable de estado es 0, es que ya hemos perdido!
-  if (remainingGuesses === 0) {
-    return <h1>Lo siento! el número era {randomNumber}</h1>
   }
 
-  // Si el array de preivousGuesses incluye en algún momento el número correcto, entonces podemos decir uqe hemos ganado
-  if (previousGuesses.includes(randomNumber)) {
-    return <h1>¡Has ganado!</h1>;
+  const handleResetGame = () => {
+    setCorrectNumber(generateRandomNumber);
+    setRemainingGuesses(10);
+    setGuesses([])
+  }
+
+  // si hemos ganado
+  if (guesses.at(-1) == correctNumber) {
+    return <div id="wrapper">
+      <h1>Game Won! Correct number was {correctNumber}</h1>
+      <button className="guessSubmit" onClick={handleResetGame}>Try Again</button>
+    </div>
+  }
+
+  // si hemos agotado los intentos
+  if (remainingGuesses === 0) {
+    return <div id="wrapper">
+      <h1>Game Lost! Correct number was {correctNumber}</h1>
+      <button className="guessSubmit" onClick={handleResetGame} >Try Again</button>
+    </div>
   }
 
   return (
     <>
-      <h1>Number guessing game</h1>
+      <h1>Number guessing game {correctNumber}</h1>
       <p>Try and guess a random number between 0   and 100.</p>
-      <p>You have {remainingGuesses} attempts to guess the right number.</p>
+      <p>You have  attempts to guess the right number.</p>
       <br />
       <div id="wrapper">
         <label htmlFor="guessField" id="guess">Guess a number</label>
         <input type="number" id="guessField" className="guessField" max={100} min={0} value={currentGuess} onChange={(e) => setCurrentGuess(e.target.value)} />
-        <button className="guessSubmit" onClick={handleClick}>Submit a Guess</button>
+        <button className="guessSubmit" onClick={handleNewGuess}>Submit a Guess</button>
 
         <div className="resultParas">
-          <p>Previous Guesses: <span className="guesses">{previousGuesses.join(' / ')}</span></p>
+          {guesses.length != 0 && <p>Previous Guesses: <span className="guesses">{guesses.join(" , ")}</span></p>}
           <p>Guesses Remaining: <span className="lastResult">{remainingGuesses}</span></p>
-          <p className="lowOrHi">{message}</p>
+          {guesses.length !=0 && <p className="lowOrHi">
+            { /**
+             * Comprobar si la última predicción es mayor o menor y mostrar un mensaje en consequencia
+             */
+             
+            guesses.at(-1) > correctNumber ? "Number is lower": "Number is higher"
+             
+             
+             }
+          </p>}
         </div>
       </div>
     </>
