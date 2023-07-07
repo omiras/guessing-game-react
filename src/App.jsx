a98eea8999b7b023972cf99119829c13b18a128b
Oscar Miras <oscar.miras@gmail.com>
2023-07-07 17:05:11 +0200

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,55 @@
 import { useState } from 'react'
 import './App.css'
 
+function generateRandomNumber() {
+  return Math.floor(Math.random() * 101);
+}
+
+const chances = 10;
+
+
 function App() {
+
+  // Número correcto
+  const [targetNumber, setTargetNumber] = useState(generateRandomNumber); // pasamos la referencia a la función y React se va a encargar de ejecutarla. Si pusieramos los parentesis, no pasaria nada, porque useState solo actualiza targetNumber la primera vez. PERO la función si que se ejecutaria
+
+  // intentos previos
+  const [previousGuesses, setPreviousGuesses] = useState([]);
+
+  // input
+  const [currentGuess, setCurrentGuess] = useState('');
+
+  // handler
+  const handleNewGuess = () => {
+    // 1. Añadir el nuevo número al array de previous Guesses
+    setPreviousGuesses([...previousGuesses, currentGuess]);
+
+    // 2. Limpiar el input
+    setCurrentGuess('');
+  }
+
+  console.log(' Me renderizo, y voy a comparar estos dos números ', currentGuess, targetNumber)
+
+  // hemos ganado? Si es nuestra última oportunidad, tenemos que comprobar esto antes de darlo por perdido.
+  if (previousGuesses.at(-1) == targetNumber) {
+    return <div id="wrapper">
+      <h1>Game Won! Correct number was {targetNumber}</h1>
+      <button>Try Again</button>
+    </div>
+  }
+
+  // hemos perdido?
+  // si el número de elementos del array es igual a la variable 'chances', hemos perdido
+  // Retornar un JSX mínimo indicando que hemos perdido y ofriencdo un botón para empezar de nuevo
+  if (chances == previousGuesses.length) {
+    return <div id="wrapper">
+      <h1>Game Lost! Correct number was {targetNumber}</h1>
+      <button>Try Again</button>
+    </div>
+  }
+
+
+
   return (
     <>
       <h1>Number guessing game</h1>
@@ -10,13 +58,22 @@ function App() {
       <br />
       <div id="wrapper">
         <label htmlFor="guessField" id="guess">Guess a number</label>
-        <input type="text" id="guessField" class="guessField" />
-        <button class="guessSubmit">Submit a Guess</button>
+        <input type="text" id="guessField" className="guessField" value={currentGuess} onChange={(e) => setCurrentGuess(e.target.value)} autoComplete='off' />
+        <button className="guessSubmit" onClick={handleNewGuess}>Submit a Guess</button>
 
-        <div class="resultParas">
-          <p>Previous Guesses: <span class="guesses"></span></p>
-          <p>Guesses Remaining: <span class="lastResult">X</span></p>
-          <p class="lowOrHi"></p>
+        <div className="resultParas">
+          <p style={{
+            visibility: previousGuesses.length == 0 ? 'hidden' : 'visible'
+          }}>Previous Guesses: <span className="guesses">{previousGuesses.join(", ")}</span></p>
+          <p>Guesses Remaining: <span className="lastResult">{chances - previousGuesses.length}</span></p>
+          <p className="lowOrHi" style={{
+            visibility: previousGuesses.length == 0 ? 'hidden' : 'visible'
+          }}>
+            {
+              // 55 > 44
+              previousGuesses.at(-1) > targetNumber ? 'Number must be lower' : 'Number must be higher'
+            }
+          </p>
         </div>
       </div>
     </>
